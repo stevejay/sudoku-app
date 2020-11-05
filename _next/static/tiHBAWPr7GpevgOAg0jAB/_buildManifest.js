651041993a8ff784688b335f295c566a4c9470eb
Steve Johns <steve@stevejay.net>
2020-11-05 10:26:38 +0000

    Updates

---
+++ _next/static/tiHBAWPr7GpevgOAg0jAB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F1bfc9850.45d7bd1c43bdf0ceeec1.js","static\u002Fchunks\u002Fpages\u002Findex-2ac9d31e02326b28b2ce.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f4f567df00ce0843964e.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
