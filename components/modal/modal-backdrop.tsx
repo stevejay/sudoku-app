74318cf82100041836983cb796c64fd9c2aaedc3
Steve Johns <steve@stevejay.net>
2020-11-06 10:13:34 +0000

    Feature/update deps 2020 11 06 (#2)
    * update minor and patch deps
    
    * update major deps

--- components/modal/modal-backdrop.tsx
+++ components/modal/modal-backdrop.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { RenderModalBackdropProps } from "react-overlays/Modal";
+import type { RenderModalBackdropProps } from "react-overlays/Modal";
 
 export const ModalBackdrop: FC<RenderModalBackdropProps> = (props) => (
   <div {...props} className="fixed inset-0 z-40 bg-black bg-opacity-75" />
